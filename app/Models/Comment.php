c85091a82c5224c4795f6c8d97f0cd64b3544854
Aldo <pedroaldo14@gmail.com>
2024-03-26 22:15:57 -0500

    feat(model): :sparkles: Structuring models, with the fillable

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['body', 'user_id', 'post_id'];
+
 }
