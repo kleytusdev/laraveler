3d1b2ce9d64524ffa51e5892b67039fb13c8dd54
Aldo <pedroaldo14@gmail.com>
2024-03-30 21:46:09 -0500

    feat(route): :sparkles: New routes file for authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
--- routes/auth.php
+++ routes/auth.php
@@ -7,17 +7,20 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Auth Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
 |
 */
 
 Route::group([
+    'middleware' => 'guest'
+], static function () {
+    Route::post('login', [AuthenticatedController::class, 'store'])->name('login');
+    Route::post('register', [RegisteredUserController::class, 'store'])->name('register');
+});
+
+Route::group([
     'middleware' => 'auth:sanctum'
 ], static function () {
-
+    Route::post('logout', [AuthenticatedController::class, 'destroy'])->name('logout');
 });
