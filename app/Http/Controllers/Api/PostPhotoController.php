d0e09b3772b305c2025fb4258a9ea1b3dbb162b4
Aldo <pedroaldo14@gmail.com>
2024-04-17 19:16:47 -0500

    feat: :sparkles: creating a symbolic link in filesystem config

--- app/Http/Controllers/Api/PostPhotoController.php
+++ app/Http/Controllers/Api/PostPhotoController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\PostPhoto;
 use Illuminate\Http\Request;
 
 class PostPhotoController extends Controller
 {
     public function storeFile(Request $request)
     {
-        dd($request->photo);
+        return PostPhoto::create([
+            'uri' => $request->file('photo')->store('posts', 'images'),
+            'post_id' => 1,
+        ]);
     }
 
     public function storeBase64(Request $request)
